a4a23d830168d620ef04613b03885e9851bab03d
= <a.payukov@iq-adv.ru>
2018-02-01 16:11:04 +0500

    local version

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -15,7 +15,7 @@ $(function () {
             autoplay: true,
             autoplayTimeout: 5000,
             autoplayHoverPause: true,
-            dots: false,
+            dots: true,
             responsive: {
                 0: {
                     items: 1
@@ -31,4 +31,37 @@ $(function () {
             }
         });
     });
+
+
+    /* ****************************** accordion ****************************** */
+
+
+        var $accordWrap = $("[data-it-accord-wrap]");
+        var $accordItem = $("[data-it-accord-item]");
+        var $accordToggle = $("[data-it-accord-toggle]");
+
+        $accordItem.hide();
+        $accordToggle.on("click", function () {
+            var x = this;
+            if ($(this).next($accordItem).css("display") === "none") {
+                $(this).closest("[data-it-accord-wrap]").find("[data-it-accord-item]").fadeOut(500);
+
+                $(this).closest("[data-it-accord-wrap]").find("[data-it-sign]").removeClass("active");
+
+            }
+
+            $(this).next($accordItem).slideToggle(200, function () {
+                //window.scrollTo(0,this.offsetTop - 200);
+            });
+            $(this).parent().find("[data-it-sign]").toggleClass("active");
+
+            /* $(this).parent().find("[]").toggle();
+             $(this).parent().find("[]").toggle();*/
+        });
+
+
 });
+
+
+
+
